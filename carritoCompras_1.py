1b9ed615e7f4798485795e80d1aa50f4798edc39
CMMProfeDuoc <ca.felipe@profesor.duoc.cl>
2023-05-31 22:55:33 -0400

    Subiendo ejercicio 1

---
+++ carritoCompras_1.py
@@ -0,0 +1,18 @@
+"""
+Cree un carrito de compras online
+para esto usted debe registrar los siguientes datos de un usuario
+
+string nombre
+lista <string> productos
+int precio_total
+
+Su programa debe mostrar al usuario la lista de productos (entregada a usted por el profesor)
+
+El usuario puede seleccionar todos los productos que desee, luego se
+mostrara el precio final
+"""
+
+#no cambiar estas lineas
+import productos
+
+lista_productos = productos.GetProductList()
