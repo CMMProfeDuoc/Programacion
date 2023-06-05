5980518d91067fe477242f5c04cad3f834a902a3
CMMProfeDuoc <ca.felipe@profesor.duoc.cl>
2023-06-05 11:09:11 -0400

    ej helados

---
+++ ejercicioHelados.py
@@ -0,0 +1,19 @@
+"""
+Cree un programa de heladeria
+El programa tiene una lista de helados
+Cada helado tiene:
+nombre, precio
+
+Además existe una lista de agregados
+cada agregado tiene
+nombre, precio
+
+HELADOS Y AGREGADOS INVENTADOS POR USUARIO
+
+El programa preguntara al usuario que sabor de helado quiere,
+luego, si quiere agregar otro (u otros) sabores
+Despues, preguntara para añadir los agregados
+
+El programa guardara cada pedido en una lista
+Preguntara por más de un helado
+"""
