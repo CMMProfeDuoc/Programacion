80168ecc1d113918eed72257b1cbb8891233ca9b
CMMProfeDuoc <ca.felipe@profesor.duoc.cl>
2023-06-08 15:55:59 -0400

    ejercicios if

---
+++ 1_if/1_2_if_else.py
@@ -0,0 +1,25 @@
+"""
+Como ya vimos, la sentencia if nos permite detectar cosas cuando
+la sentencia es VERDADERA (True)
+
+Tambien existe una sentenia llamada <else> que permite tomar
+acciones en caso de que la sentencia sea <if>
+
+if (sentencia):
+    TRUE
+else:
+    FALSE
+
+Probemos con un programa que detecta si un numero es mayor o menor que 10
+"""
+
+numero = int(input("Ingrese un numero: "))
+if (numero >= 10):
+    print("El numero es mayor o igual a 10")
+else:
+    print("El numero es menor que 10")
+
+"""
+    elif <<< to do
+"""
+
--- test.py
+++
@@ -1,1 +0,0 @@
-print("Prueba de git")
