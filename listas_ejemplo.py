1e1ba92f7b924485cab094cebf67c5ad7e8c8865
CMMProfeDuoc <ca.felipe@profesor.duoc.cl>
2023-06-12 11:23:16 -0400

    lista

---
+++ listas_ejemplo.py
@@ -0,0 +1,7 @@
+nombres = ["Pepito","Juanito","Pedrito","Juanita","Panchita","Gabriel","Juliancito"]
+
+lista_mascotas = ["dino","lalito","peyo","zanahoria"]
+
+lista_numeros = [1,41,2,4,1,5,6,3,5,2]
+
+lista_mixta = ["sk",1,3,5,"so"]
