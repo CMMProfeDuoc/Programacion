e675d266ce1af26c84f89b03ffda016216ada10d
CMMProfeDuoc <ca.felipe@profesor.duoc.cl>
2023-06-13 16:58:09 -0400

    ej 5 func

---
+++ 5_funciones_import/5_3_ej_func.py
@@ -0,0 +1,18 @@
+# NO MODIFICAR -----
+import listas
+lista_nombres = listas.getNameList()
+# NO MODIFICAR -----
+
+"""
+1. Cree una funcion que reciba una letra y cuente cuantos
+nombres hay con esa letra
+
+2. Cree un programa que reciba una lista y una palabra y
+verifique si ese elemento se repite en la lista
+
+3. Cree un programa que reciba SOLO una lista y verifique
+si hay elemnentos repetidos
+"""
+
+
+print(lista_nombres)
