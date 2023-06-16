f99e047d5847f47b880dd0296d21c982e4e8fae2
CMMProfeDuoc <ca.felipe@profesor.duoc.cl>
2023-06-16 17:44:05 -0400

    example

--- ej_avanzados/sistemaLogin_v2.py
+++ ej_avanzados/sistemaLogin_v2.py
@@ -16,3 +16,32 @@ Cree las siguientes funciones:
 import michiHerramientas as Michi
 contactos = Michi.GetUserList()
 #editar de aqui en adelante ---------------
+
+def imprmirLista (lista:list, show_pass:bool=True) -> None:
+    for l in lista:
+        print(l["nombre"],end=" ")
+        if (show_pass):
+            print(l["contraseña"])
+        else:
+            con = "*" * len(l["contraseña"])
+            print(con)
+
+def addUser (lista:list,nombre:str,password:str="1234") -> None:
+    if (not buscarUser(lista,nombre)):
+        lista.append({"nombre":nombre,"contraseña":password})
+
+def buscarUser (lista:list, user_name:str) -> bool:
+    for l in lista:
+        if (user_name == l["nombre"]):
+            return True
+    return False
+
+addUser(contactos,"lalalala") 
+imprmirLista(contactos,False)
+
+#-------------
+#MENU
+#opciones = ["imprimir","añadir usuario","buscar usuario","salir"]
+#while(True):
+    #pass
+    
