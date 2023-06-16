c47f1cfd78968d0c2b8ab62ebbf86fb50871e3ab
CMMProfeDuoc <ca.felipe@profesor.duoc.cl>
2023-06-16 16:50:33 -0400

    login v2

---
+++ ej_avanzados/sistemaLogin_v2.py
@@ -0,0 +1,18 @@
+"""
+Cree un sistema de Login
+Para esto, usted recibe una lista de usuarios, los cuales tienen
+<nombre_usuario> <contraseña>
+
+Cree las siguientes funciones:
+
+1. Buscar un usuario
+
+2. Ingresar un usuario a la lista
+
+3. Mostrar todos los usuarios
+"""
+
+#no cambiar estas lineas ------------------
+import michiHerramientas as Michi
+contactos = Michi.GetUserList()
+#editar de aqui en adelante ---------------
