e5bf6720aab0a7ac132697cee6f66858af4130d9
unknown <Cetecom@SJAPCA03LC1000.duoc.acad>
2024-05-07 10:38:09 -0400

    ej3 update

--- 3_array/ej_03.py
+++ 3_array/ej_03.py
@@ -1,3 +1,5 @@
 """
 Cree un programa que reciba numeros positivos, del 0 al 15. el programa se detiene cuando se ingresa un numero negativo. Luego muestre cuanto se repite cada numero.
+
+DESAFIO: Maximo 2 ciclos.
 """
