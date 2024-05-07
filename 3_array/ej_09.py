5f212d9af5b50dcf88223b4764e5530eb58d4e38
CMMProfeDuoc <Cetecom@SJAPCA03LC1000.duoc.acad>
2024-05-07 11:21:44 -0400

    ej 9

---
+++ 3_array/ej_09.py
@@ -0,0 +1,3 @@
+"""
+Crea un programa que pida un número al usuario un número de mes (por ejemplo, el 4) y diga cuántos días tiene (por ejemplo, 30) y el nombre del mes. Debes usar un par de listas. Para simplificarlo vamos a suponer que febrero tiene 28 días.
+"""
