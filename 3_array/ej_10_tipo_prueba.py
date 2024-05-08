5f5c30b31844cbb8390e65cec65bd4f551c77dd4
CMMProfeDuoc <132609289+CMMProfeDuoc@users.noreply.github.com>
2024-05-08 09:19:13 -0400

    ejercicio 10
    si hacen un ordenamiento que sea O(nlogn) y justifica porque ese ordenamiento funciona asi, 10 para prueba

---
+++ 3_array/ej_10_tipo_prueba.py
@@ -0,0 +1,11 @@
+"""
+ESTE EJERCICIO ES LA COTA SUPERIOR DE UNA PRUEBA
+
+Suponga que tiene una lista desordenada de numeros enteros.
+Su trabajo es ordenar la lista.
+No puede usar funciones de ordenamiento de python.
+Si una funcion de ordenamiento de python, debe explicar como funciona el proceso de ordenamiento.
+
+Opcional:
+Explique cuanto se demoraria, ¿y porque?
+"""
