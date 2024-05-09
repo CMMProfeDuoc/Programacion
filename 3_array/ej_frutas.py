1143e03f71bbfbdbf537f6544e93526bb1cf22d5
CMMProfeDuoc <132609289+CMMProfeDuoc@users.noreply.github.com>
2024-05-09 10:14:49 -0400

    frutas frutas

---
+++ 3_array/ej_frutas.py
@@ -0,0 +1,23 @@
+"""
+    Un colector de frutas varias, recolecta frutas y anota el tipo de fruta y el peso (en kg) de las frutas en dos listas
+    La primera lista, contiene los tipos de frutas recolectadas
+    La segunda lista, contiene el peso de las frutas
+
+    Usted debe crear un programa que reciba estas listas e indique el valor de la venta del recolector, segun lo siguiente:
+
+        -> kg de manzanas => $1500
+        -> kg de tomate => $3000
+        -> kg de limon => $700
+        -> kg de sandia => $7000
+        -> kg de naranja => $800
+
+        Cualquier otra fruta no se considera para la venta.
+"""
+
+frutas = [
+    'manzana','manzana','sandia','pera','limon','tomate'
+]
+
+kilos = [
+    1, 0.5, 2, 2.2, 1.5, 1.2
+]
