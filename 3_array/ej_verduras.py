dc670acaff563ba5f979daf759d374892011e3ff
CMMProfeDuoc <132609289+CMMProfeDuoc@users.noreply.github.com>
2024-05-13 15:28:18 -0400

    Add files via upload

---
+++ 3_array/ej_verduras.py
@@ -0,0 +1,11 @@
+"""
+    Considere que tiene una lista de sabores
+    de verduras y su peso en kg.
+    Indique cuantos kilos de cada verdura hay.
+    Usted sabe que solo hay 3 verduras.
+        apio, lechuga, rabano
+
+    El peso solo puede ser un numero positivo, de un digito (entre 0 y 9)
+"""
+
+lista_verduras = ['lechuga 5','apio 6','lechuga 1','rabano 3','apio 2', 'apio 1']
