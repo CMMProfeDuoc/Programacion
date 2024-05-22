dde471ca1ecd455b94e7f8c02791e37ecc9797c0
CMMProfeDuoc <132609289+CMMProfeDuoc@users.noreply.github.com>
2024-05-22 09:14:48 -0400

    Create ej_01.py

---
+++ 3_array/3_5_multiDimension_array/ej_01.py
@@ -0,0 +1,18 @@
+menu = [
+    ['cazuela',3000],
+    ['sopaipilla',100],
+    ['porotos',3000],
+]
+
+menu.append(['completo',1000])
+
+#imprimir lista
+for item in menu:
+    nombre = item[0]
+    precio = item[1]
+    print(nombre,' $',precio)
+
+# Hacer menu
+    # 1. Crear menu para 'seleccionar' item
+    
+    # 2. Dar opcion para agregar cosas al menu (con verificacion de datos)
