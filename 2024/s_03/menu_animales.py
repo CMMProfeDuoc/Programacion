3e9dc8dc0fee232ff4514a39ff480362825359f1
CMMProfeDuoc <132609289+CMMProfeDuoc@users.noreply.github.com>
2024-05-28 11:35:23 -0400

    Create menu_animales.py

---
+++ 2024/s_03/menu_animales.py
@@ -0,0 +1,48 @@
+# SECCION 003 D
+# V 1
+# fecha: 28/05
+# hagan los ejercicios
+
+"""
+    1. Mostrar la lista numerada de animales (para poder seleccionar)
+    2. Poder 'filtrar' la lista de animales, segun el tipo
+"""
+
+lista_animales = [
+    {
+        'nombre': 'pepito',
+        'tipo' : 'gato',
+        'peso' : 7.2, 
+        'color' : 'naranjo',
+    },
+    {
+        'nombre': 'jaimico',
+        'tipo' : 'mandril',
+        'peso' : 20.2, 
+        'color' : 'plomo',
+    },
+    {
+        'nombre': 'soy la comadreja',
+        'tipo' : 'comadreja',
+        'peso' : 7.2, 
+        'color' : 'rojo',
+    },
+    {
+        'nombre': 'mojojojo',
+        'tipo' : 'mono',
+        'peso' : 25.3, 
+        'color' : 'verde',
+    },
+    {
+        'nombre': 'mordecai',
+        'tipo' : 'azulejo',
+        'peso' : 2.2, 
+        'color' : 'azul',
+    },
+    {
+        'nombre': 'skips',
+        'tipo' : 'yeti',
+        'peso' : 200, 
+        'color' : 'blanco',
+    },
+]
