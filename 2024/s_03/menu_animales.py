db4ee75f94f3c4ce0ac7e64886fd3e944c7e5f99
CMMProfeDuoc <132609289+CMMProfeDuoc@users.noreply.github.com>
2024-05-30 11:27:46 -0400

    Update menu_animales.py

--- 2024/s_03/menu_animales.py
+++ 2024/s_03/menu_animales.py
@@ -4,10 +4,11 @@
 # hagan los ejercicios
 
 """
-    1. Mostrar la lista numerada de animales (para poder seleccionar)
-    2. Poder 'filtrar' la lista de animales, segun el tipo
+    ✔ 1. Mostrar la lista numerada de animales (para poder seleccionar)
+    
+    ✔ 2. Poder 'filtrar' la lista de animales, segun el tipo
 
-    3. (jueves) Agregar animales a la lista (deben tener los mismos datos,
+    3. Agregar animales a la lista (deben tener los mismos datos,
     nombre, tipo, peso, color)
 """
 
@@ -48,4 +49,71 @@ lista_animales = [
         'peso' : 200, 
         'color' : 'blanco',
     },
+    {
+        'nombre': 'juanito',
+        'tipo' : 'gato',
+        'peso' : 6, 
+        'color' : 'negro',
+    },
+    {
+        'nombre': 'pedrito',
+        'tipo' : 'mapache',
+        'peso' : 5, 
+        'color' : 'cafe',
+    },
+    {
+        'nombre': 'rocket',
+        'tipo' : 'mapache',
+        'peso' : 5, 
+        'color' : 'plomo',
+    },
 ]
+
+opciones_menu = [
+    'salir',
+    'mostrar y seleccionar',
+    'filtrar por tipo',
+    'agregar (WIP)',
+]
+
+while (True):
+    for i, opcion in enumerate(opciones_menu):
+        print(i, opcion)
+
+    while (True):
+        sel = int(input(">> "))
+        if (sel in range(len(opciones_menu))):
+            #ARREGLAR BUG DE SELECCION DE MENU
+            break
+
+    #1
+    #mostrar animales de la lista
+    for i, animal in enumerate(lista_animales):
+        print(i+1,'. ',animal['nombre'],' | ',animal['tipo'], sep='')
+
+    if (sel == 1):
+        #1 seleccionar un animal y mostrar detalle
+        while (True):
+            sel = int(input(">> "))-1
+            if (sel in range(len(lista_animales))):
+                animal = lista_animales[sel] #asignar variable para mejor legibilidad
+                for llave in animal.keys():
+                    print(llave,': ',animal[llave],sep='')
+                print('-'*10)
+                break
+
+    if (sel == 2):
+        #2 - filtar
+        print('Filtrar: ')
+        lista_filtrada = []
+        filtro = input('indique <tipo> a filtrar: ').lower()
+        for animal in lista_animales:
+            if (animal['tipo'].lower() in filtro):
+                lista_filtrada.append(animal)
+        print('-'*10)
+        for i, animal in enumerate(lista_filtrada):
+            print(i+1,'. ',animal['nombre'],' | ',animal['tipo'], sep='')
+
+    if (sel == 0):
+        print("adios")
+        break
