a0ed40f1c631f40549b18bd87c32433080d83a67
CMMProfeDuoc <132609289+CMMProfeDuoc@users.noreply.github.com>
2024-05-30 16:42:24 -0400

    Update menu_mascotas.py

--- 2024/s_02/menu_mascotas.py
+++ 2024/s_02/menu_mascotas.py
@@ -9,26 +9,32 @@
             y mostrar detalle del animal 
             ✔ >> solucionar posibles errores en seleccion
         
-        - Guardar los animales seleccionados
-    
-        - Mostrar animales seleccionados
-
-        ✔ Poder 'filtrar' la lista de animales, segun el tipo
+        ✔ Guardar los animales seleccionados
+            
+    ✔ Mostrar animales seleccionados
+        opciones:
+        - limpiar lista
+        - ver detalle de un animal
+        - quitar item de lista
         
-        - Agregar animales a la lista (deben tener los mismos datos,
+    ✔ Poder 'filtrar' la lista de animales, segun el tipo
+        - Agregar a la selccion
+        
+    ✔ Agregar animales a la lista (deben tener los mismos datos,
         nombre, tipo, peso, color)
+        - validar datos
 
-        ✔ Crear Funcion que determine si una seleccion es valida
+    ✔ Crear Funcion que determine si una seleccion es valida
             El usuario tiene un rango de seleccion, y la funcion retorna True o False
             dependiendo si la seleccion es valida o no
 
-        - Eliminar Animal
+    - Eliminar Animal
             - mostrar la lista
             - seleccionar que cosa eliminar
 
-        - Modificar el dato de un animal
+    - Modificar el dato de un animal
 
-        - Agregar un dato (llave : valor) a un animal           
+    - Agregar un dato (llave : valor) a un animal           
 
     
 
@@ -42,11 +48,11 @@ def mostrarAnimalLista (lista:list) -> None:
     for i, animal in enumerate(lista):
         print(i+1,'. ', animal['nombre'],' => ', animal['tipo'], sep='')
 
-def selOpcion (lista:list) -> int:
+def selOpcion (lista:list,desfase:int=0) -> int:
     while (True):
         sel = input(">> ")
         if (sel.isnumeric()):
-            sel = int(sel)-1
+            sel = int(sel)-desfase
             if (sel in range(len(lista))):
                 break
     return sel
@@ -114,29 +120,40 @@ lista_animales = [
     },
 ]
 
+animales_guardados = []
+
 opciones_menu = [
+    'salir', #con 0 se sale
     'mostrar lista animales y seleccionar',
     'seleccionar por tipo',
     'agregar animales',
+    'mostrar animales guardados'
 ]
 
 while (True): #WHILE MENU
     mostrarLista(opciones_menu)
     sel = selOpcion(opciones_menu)
 
-    if (sel == 0):
+    if (sel==0):
+        break
+    
+    if (sel == 1):
         mostrarAnimalLista(lista_animales)
         print()
-        sel = selOpcion(lista_animales)
+        sel = selOpcion(lista_animales,1)
 
         animal_seleccionado = lista_animales[sel]
         print('-'*10)
         for llave in animal_seleccionado.keys():
             print(llave,'=>',animal_seleccionado[llave])
+
+        if (animal_seleccionado not in animales_guardados):
+            animales_guardados.append(animal_seleccionado)
+
         continue            
     #end if 0
 
-    if (sel == 1):
+    if (sel == 2):
         lista_filtrada = []
         filtro = input('ingrese tipo a filtrar: ').lower()
         for animal in lista_animales:
@@ -148,6 +165,18 @@ while (True): #WHILE MENU
         continue
     #end if 1
         
-    if (sel == 2):
-        print("OPCION AUN NO LISTA")
+    #agregar animal a la lista
+    if (sel == 3):
+        #Falta crear validacion de datos
+        lista_animales.append(
+            {
+                'nombre': input('ingrese nombre: '),
+                'tipo' : input('ingrese tipo: '),
+                'peso' : float(input('ingrese peso: ')), 
+                'color' : input('ingrese color: '),
+            }
+        )
+
+    if (sel == 4):
+        mostrarAnimalLista(animales_guardados)
         continue
