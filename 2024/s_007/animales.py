d3c5796468679332513f3c1cc333abf8b05a28ab
CMMProfeDuoc <132609289+CMMProfeDuoc@users.noreply.github.com>
2024-05-31 14:48:21 -0400

    Create animales.py

---
+++ 2024/s_007/animales.py
@@ -0,0 +1,52 @@
+# s007
+
+"""
+    - Crear un menu para seleccionar una de las siguientes acciones:
+
+        - Mostrar animales en la lista, de forma numerada, mostrando "nombre => tipo"
+        Para poder seleccionar un animal y mostrar los detalles del animal.
+
+        - Seleccionar un animal de la lista y guardarlo en otra lista separada, la nueva lista no puede tener repetidos.
+
+        - Filtrar animales segun el <tipo>. El <tipo> lo ingresa el usuario.
+"""
+
+lista_animales = [
+    {
+        'id' : 1,
+        'nombre' : 'panxito',
+        'tipo' : 'panda',
+        'color' : 'blanco con amarillo',
+    },
+    {
+        'nombre' : 'pepito',
+        'tipo' : 'gato',
+        'color' : 'verde',
+        'id' : 2,
+    },
+    {
+        'nombre' : 'pepita',
+        'tipo' : 'pig',
+        'color' : 'rosada',
+        'id' : 3,
+    },
+    {
+        'nombre' : 'rigby',
+        'tipo' : 'mapache',
+        'color' : 'cafe',
+        'id' : 4,
+    },
+    {
+        'nombre' : 'mordecai',
+        'tipo' : 'azulejo',
+        'color' : 'azul',
+        'id' : 5,
+    },
+    {
+        'nombre' : 'pedrito',
+        'tipo' : 'mapache',
+        'color' : 'plomo',
+        'id' : 6,
+    },
+]
+
