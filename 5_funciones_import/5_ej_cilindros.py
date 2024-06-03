85994a842a4a67bfdad315eea9fa1be2f1057470
CMMProfeDuoc <132609289+CMMProfeDuoc@users.noreply.github.com>
2024-06-03 15:36:51 -0400

    Update 5_ej_cilindros.py

--- 5_funciones_import/5_ej_cilindros.py
+++ 5_funciones_import/5_ej_cilindros.py
@@ -1,17 +1,15 @@
 """
-Cree un programa que calcule el area de distintas figuras.
-Para eso, cree una funcion que reciba un numero (el radio) y
-retorne el area del circulo ( A = r*r*3.1415 )
+    Cree un dos funciones para calcular el area de
+    un cilindro.
+    
+    Primero cree una funcion que calcule el area de un
+    circulo (Acirculo = radio * radio * 3.1415)
 
-Luego cree una funcion que calcule el area de un cilindro, recibiendo el radio y el
-alto de la figura.
+    Luego use la Fx anterior para calcular el area
+    del cilindro
+    Acilindro = (2 * A_circulo) + (altura * radio)
 
-(A = Acirculo * altura)
-
-*Si el area es 'imposible' (menor a 0)
-Retornar -1
-
-Se le otorgara una lista de diccionarios con las mediadas de los cilindros
+    El usuario ingresa la altura y el radio
 """
 
 def areaCirculo (radio:float) -> float:
@@ -19,26 +17,15 @@ def areaCirculo (radio:float) -> float:
     return area
 
 def areaCilindro (radio:float, altura:float) -> float:
-    area = areaCirculo(radio) * altura
+    area = (2 * areaCirculo(radio)) + (radio * altura)
     return area
 
-lista_cilindros = [
-    {
-        'radio':5,
-        'altura':6,
-    },
-    {
-        'radio':2.1,
-        'altura': 5,
-    },
-    {
-        'radio':3,
-        'altura':18,
-    },
-]
 
-for cilindro in lista_cilindros:
-    cilindro.update({
-        'area':areaCilindro(cilindro['radio'],cilindro['altura'])
-    })
-    print(cilindro)
+cilindro = {
+    'radio' : float(input('radio: ')),
+    'altura' : float(input('altura: '))
+}
+
+cilindro['area'] = areaCilindro(cilindro['radio'],cilindro['altura'])
+
+print(cilindro)
