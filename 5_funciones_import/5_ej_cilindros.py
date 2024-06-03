b9bd8a5c1f75db7e22619d941fd87e314af1251a
CMMProfeDuoc <132609289+CMMProfeDuoc@users.noreply.github.com>
2024-06-03 09:57:42 -0400

    Update 5_ej_cilindros.py

--- 5_funciones_import/5_ej_cilindros.py
+++ 5_funciones_import/5_ej_cilindros.py
@@ -3,9 +3,13 @@ Cree un programa que calcule el area de distintas figuras.
 Para eso, cree una funcion que reciba un numero (el radio) y
 retorne el area del circulo ( A = r*r*3.1415 )
 
-
 Luego cree una funcion que calcule el area de un cilindro, recibiendo el radio y el
 alto de la figura.
+
+(A = Acirculo * altura)
+
+*Si el area es 'imposible' (menor a 0)
+Retornar -1
 
 Se le otorgara una lista de diccionarios con las mediadas de los cilindros
 """
