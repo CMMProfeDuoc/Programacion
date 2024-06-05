8f8369a254083305d383b41ca27cb2f54b252450
CMMProfeDuoc <132609289+CMMProfeDuoc@users.noreply.github.com>
2024-06-05 13:16:41 -0400

    Create ej_texto.py

---
+++ 5_funciones_import/ej_texto.py
@@ -0,0 +1,8 @@
+"""
+Cree una funcion que reciba un texto y devuelva un diccionario
+con la frecuencia (veces que se repite) cada caracter del texto.
+
+Crear otra funcion, que reciba un diccionario (el anterior) y muestre la letra que más se repite.
+"""
+
+texto = 'Habia una vez un perrito que se llamaba pafuera y le dijieron pafuera padentro y exploto'
