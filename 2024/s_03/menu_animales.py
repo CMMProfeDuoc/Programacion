a8d704eda681d03cebf1ac19fad16898076f304a
CMMProfeDuoc <132609289+CMMProfeDuoc@users.noreply.github.com>
2024-06-05 12:03:21 -0400

    filtro agregado

--- 2024/s_03/menu_animales.py
+++ 2024/s_03/menu_animales.py
@@ -1,32 +1,53 @@
 # SECCION 003 D
-# V 1.3
+# V 1.7
 # fecha: 5/6
 # hagan los ejercicios
 
 """
     ✔ 1. Mostrar la lista numerada de animales (para poder seleccionar)
     
-    >> Crear funcion para filtrar por llave
+    ✔ Crear funcion para filtrar por llave
         La funcion recibe una lista y un filtro
         Y devuelve una lista con los elementos filtrados
         (recordar que los elementos no se pueden repetir)
 
-    3. Agregar animales a la lista (deben tener los mismos datos,
+    >> Agregar animales a la lista (deben tener los mismos datos,
     nombre, tipo, peso, color)
 """
+
+def filtrar (lista_objetivo:list[dict],llave:str,filtro:str) -> list:
+    lista_filtrada = []
+    for elemento in lista_objetivo:
+        if (str(elemento[llave]) in str(filtro)):
+            lista_filtrada.append(elemento)
+    return lista_filtrada
+
+def filtrarTodo (lista_objetivo:list[dict], filtro:str) -> list:
+    lista_filtrada = []
+    lista_final = []
+    for elemento in lista_objetivo:
+        for key in elemento.keys():
+            lista_filtrada += filtrar(lista_objetivo,key,filtro)
+
+    #quitar repetidos
+    for elemento in lista_filtrada:
+        if (elemento not in lista_final):
+            lista_final.append(elemento)
+
+    return lista_final
 
 def seleccionMenu (menu:dict) -> str:
     for opcion in menu.keys():
         print(opcion, '=>', menu[opcion])
 
     opc_sel = input('ingrese comando: ')
+
+    #verificar que la opcion sea valida
+    
     return opc_sel
 
 
-def mostrarListaAnimales (
-        lista_animales:list[dict],
-        nombre_lista:str,
-        ) -> None:
+def mostrarListaAnimales (lista_animales:list[dict],nombre_lista:str) -> None:
     print('--',nombre_lista,'--')
     for i, animal in enumerate(lista_animales):
         print(i, animal['nombre'])
@@ -90,41 +111,39 @@ lista_animales = [
 ]
 
 
-
-
 while (True): #programa
 
     opciones_menu = {
         'salir':['s','salir','chao'],
         'ver animales':['v','ver'],
-        'filtrar':['/f','f','filtrar','filter']
+        'filtrar':['/f','f','filtrar','filter'],
+        'filtrar por tipo':['ft','tipo'], #<<
+        'agregar':['a','add'], #<<
+        'quitar':['q','quitar','eliminar','delete'],#<<
+        'modificar':['m','mod','modificar'] #<<
     }
 
+    #Agregar opciones marcadas
+    
     sel = seleccionMenu(opciones_menu)
 
     if (sel in opciones_menu['ver animales']):
-        #1
-        #mostrar animales de la lista
         mostrarListaAnimales(lista_animales,'Animales')
-        #1 seleccionar un animal y mostrar detalle
         while (True):
             sel = int(input(">> "))-1
             if (sel in range(len(lista_animales))):
-                animal = lista_animales[sel] #asignar variable para mejor legibilidad
+                animal = lista_animales[sel] 
                 for llave in animal.keys():
                     print(llave,': ',animal[llave],sep='')
                 print('-'*10)
                 break
 
     if (sel in opciones_menu['filtrar']):
-        #2 - filtar
         print('Filtrar: ')
-        lista_filtrada = []
-        filtro = input('indique <tipo> a filtrar: ').lower()
-        for animal in lista_animales:
-            if (animal['tipo'].lower() in filtro):
-                lista_filtrada.append(animal)
-        print('-'*10)
+        lista_filtrada = filtrarTodo(
+            lista_animales,
+            input('ingrese filtro: ')
+            )
         mostrarListaAnimales(lista_filtrada,'Resultado')
 
     if (sel in opciones_menu['salir']):
