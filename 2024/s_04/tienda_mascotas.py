6d7b1af67a5390dfce55d347913d217b21a40f05
CMMProfeDuoc <132609289+CMMProfeDuoc@users.noreply.github.com>
2024-06-06 12:43:41 -0400

    funciones y filtros

--- 2024/s_04/tienda_mascotas.py
+++ 2024/s_04/tienda_mascotas.py
@@ -1,5 +1,5 @@
-# V 1.2
-# 30/5
+# V 1.4
+# 6/6
 
 """
     4. Añadir prevencion de errores al seleccionar cosas del menu
@@ -12,6 +12,29 @@
     3. Agregar un animal, que debe tener, por lo menos, los sig atributos
     nombre, tipo, color, peso
 """
+
+def imprimirListaAnimales (lista:list[dict], nombreLista:str='Lista') -> None:
+    print('--',nombreLista,'--')
+    for i, animal in enumerate(lista):
+        print(i+1, animal['nombre'],animal['tipo'])
+    print('-'*10)
+
+def selecItemLista (lista:list, nombreLista:str) -> int:
+    print('--',nombreLista,'--')
+    for i, item in enumerate(lista):
+        print(i+1, item)
+
+    while (True):
+        selec = int(input('>> '))-1
+        if (selec in range(len(lista))):
+            return selec
+        
+def filtrarListaDict (lista:list[dict], llave:str, filtro:str) -> list[dict]:
+    lista_filtrada = []
+    for elemento in lista:
+        if (str(elemento[llave]) == str(filtro)):
+            lista_filtrada.append(elemento)
+    return lista_filtrada
 
 lista_animales = [
     {
@@ -66,17 +89,14 @@ opciones_menu = [
 ]
 
 while (True):
-    for i, opcion in enumerate(opciones_menu):
-        print(i, opcion)
-    sel_opcion = int(input(">> "))
+    sel_opcion = selecItemLista(opciones_menu,'Menu')
 
     if (sel_opcion == 0):
         break
 
     #1 - mostrar y seleccionar
     if (sel_opcion == 1):
-        for i, animal in enumerate(lista_animales):
-            print(i+1,'. ', animal['nombre'],' => ',animal['tipo'], sep='')
+        imprimirListaAnimales(lista_animales,'Animales')
 
         sel = int(input(">> "))-1
         animalSel = lista_animales[sel]
@@ -86,14 +106,15 @@ while (True):
 
     #2 - filtrar por tipo
     if (sel_opcion == 2):
-        lista_filtrada = []
-        filtro = input("ingrese <tipo> filtro: ").lower()
-        for animal in lista_animales:
-            if (animal['tipo'] == filtro):
-                lista_filtrada.append(animal)
+        lista_filtros = ['nombre','color','tipo','peso']
+        sel_filtro = selecItemLista(lista_filtros,'Llaves Busqueda')
+        lista_filtrada = filtrarListaDict(
+            lista_animales,
+            lista_filtros[sel_filtro],
+            input('ingrese filtro: ')
+        )
+        imprimirListaAnimales(lista_filtrada,'Resultado Filtro')
 
-        for i, animal in enumerate(lista_filtrada):
-            print(i+1,'. ', animal['nombre'],' => ',animal['tipo'], sep='')
 
     if (sel_opcion == 3):
         print("trabajo en progreso")
