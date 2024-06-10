286969819900830ee684b07109f90e2337c0fdac
CMMProfeDuoc <132609289+CMMProfeDuoc@users.noreply.github.com>
2024-06-10 15:24:51 -0400

    Create ejercicio_multiplos.py

---
+++ 5_funciones_import/ejercicio_multiplos.py
@@ -0,0 +1,8 @@
+"""
+Cree una funcion que reciba un numero y entrege todos los numeros que sean divisores del numero (entre 1 y el numero)
+[un divisor es un numero que deja resto 0]
+
+Luego, usando la funcion anterior, creee un diccionario con todos los numeros entre el 1 y el 100, que tenga el numero y la cantidad de divisores.
+
+Luego indique que numeros son numeros primos.
+"""
